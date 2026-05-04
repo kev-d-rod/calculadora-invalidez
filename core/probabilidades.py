561d4ce8fb5d0bfa79ca8694da05578dfc54226c
kev-d-rod <kevin.rodriguezpz@udlap.mx>
2026-05-04 17:03:47 -0600

    ahora sí bien ascendientes

--- core/probabilidades.py
+++ core/probabilidades.py
@@ -170,6 +170,6 @@ def calcular_mcsi(edad_trabajador, salarios_actualizados, conyuge, hijos, edades
                 )
 
         pnsi = 11.81 * suma_total_padres * FACBI
-        mcsi_final = b1_j(1)
+        mcsi_final = pnsi * (1 + 0.02 + 0.01)
 
     return mcsi_final
