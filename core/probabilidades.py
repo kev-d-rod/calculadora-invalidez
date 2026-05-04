60d72755482240b25c4ef5d8d39f85081d8914bb
kev-d-rod <kevin.rodriguezpz@udlap.mx>
2026-05-04 15:23:10 -0600

    agregamos inc con hijos conyuge y

--- core/probabilidades.py
+++ core/probabilidades.py
@@ -128,7 +128,7 @@ def calcular_mcsi(edad_trabajador, salarios_actualizados, conyuge, hijos, edades
         tot_kpysubsum = (kpy_conyuge[:min_len] * sumab1[:min_len]) + ((1 - kpy_conyuge[:min_len]) * sumab2[:min_len])
 
         min_len_final = min(len(tot_kpysubsum), len(kpxvk_trabajador))
-        pnsi = 11.81 * np.sum(tot_kpysubsum[:min_len_final] * kpxvk_trabajador[:min_len_final]) * FACBI
+        pnsi = 11.81 * np.sum(tot_kpysubsum[:min_len_final] * kpxvk_trabajador[:min_len_final]) * FACBI * (1 + INC)
         mcsi_final = 11.81 * np.sum(tot_kpysubsum[:min_len_final] * kpxvk_trabajador[:min_len_final]) * (1 + 0.02 + 0.01)
 
     # CASO 4: SIN CÓNYUGE, SIN HIJOS, CON PADRES
