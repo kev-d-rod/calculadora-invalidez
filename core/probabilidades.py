684dc9cd49b66b2866f20c3d3b84ded83dcd187f
kev-d-rod <kevin.rodriguezpz@udlap.mx>
2026-05-04 16:57:58 -0600

    cehcarguanct

--- core/probabilidades.py
+++ core/probabilidades.py
@@ -164,6 +164,6 @@ def calcular_mcsi(edad_trabajador, salarios_actualizados, conyuge, hijos, edades
                 )
 
         pnsi = 11.81 * suma_total_padres * FACBI
-        mcsi_final = b1_j(1)
+        mcsi_final = cuantia_mensual_base_gral
 
     return mcsi_final
