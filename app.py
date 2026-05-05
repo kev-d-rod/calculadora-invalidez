703444a8fb3fc60439fb02bbdd6378ce26b2d070
kev-d-rod <kevin.rodriguezpz@udlap.mx>
2026-05-05 00:21:59 -0600

    DE VERDAD CREO EN TI SEÑOR JESUS

--- app.py
+++ app.py
@@ -258,6 +258,6 @@ if st.button("Calcular monto constitutivo"):
         col1, col2 = st.columns(2)
         
         with col1:
-            st.metric("Monto Constitutivo (PBSS)", f"S{resultado_pbss:,.2f}")
+            st.metric("Monto Constitutivo (MCSS)", f"S{resultado_pbss:,.2f}")
         with col2:
             st.metric("Monto Constitutivo (MCSI)", f"S{resultado_pbsi:,.2f}")
