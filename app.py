dab930eb3d5d9203ab86076859aadf767a41214e
kev-d-rod <kevin.rodriguezpz@udlap.mx>
2026-05-04 20:28:54 -0600

    si

--- app.py
+++ app.py
@@ -248,6 +248,10 @@ if st.button("Calcular monto constitutivo"):
             df_desercion=tabla_desercion
         )
         st.write("PBSS calculado:", resultado_pbss)
+        if st.button("🔄 Limpiar cache"):
+            st.cache_data.clear()
+
+        st.write("FORZAR RECARGA V2")
 
         st.subheader("Resultados del Cálculo")
         col1, col2 = st.columns(2)
